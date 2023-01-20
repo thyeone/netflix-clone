eb7dfe7f3396d022f4d8de36738fd0731239e2bf
thyeone <thyeonee@gmail.com>
2023-01-20 20:06:53 +0900

    Seperate Banner Component

---
+++ src/Components/Banner.tsx
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+import { IGetMoviesResult } from "../typing";
+import { makeImagePath } from "../utils/utils";
+
+interface IProps {
+  increaseIndex: () => void;
+  nowPlaying?: IGetMoviesResult;
+}
+
+function Banner({ increaseIndex, nowPlaying }: IProps) {
+  return (
+    <Wrapper
+      onClick={increaseIndex}
+      bgPhoto={makeImagePath(nowPlaying?.results[0].backdrop_path || "")}
+    >
+      <Title>{nowPlaying?.results[0].original_title}</Title>
+      <Overview>{nowPlaying?.results[0].overview}</Overview>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div<{ bgPhoto: string }>`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 60px;
+  background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)),
+    url(${(props) => props.bgPhoto});
+  background-size: cover;
+`;
+
+const Title = styled.h2`
+  font-size: 72px;
+  font-weight: 700;
+  margin-bottom: 20px;
+`;
+
+const Overview = styled.p`
+  font-size: 20px;
+  font-weight: 300;
+  line-height: 25px;
+  width: 50%;
+`;
+
+export default Banner;
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,12 +1,12 @@
-import { motion, AnimatePresence } from "framer-motion";
 import { useState } from "react";
 import { useQuery } from "react-query";
 import { useMatch, useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import Banner from "../Components/Banner";
 import MovieModal from "../Components/MovieModal";
 import Slider from "../Components/Slider";
 import { IGetMoviesResult } from "../typing";
-import { getMovies } from "../utils/api";
+import { NowPlaying } from "../utils/api";
 import { makeImagePath } from "../utils/utils";
 
 const offset = 6;
@@ -16,7 +16,7 @@ function Home() {
   const movieMatch = useMatch("/movies/:movieId");
   const { isLoading, data: nowPlaying } = useQuery<IGetMoviesResult>(
     ["movies", "nowPlaying"],
-    getMovies
+    NowPlaying
   );
   const [index, setIndex] = useState(0);
   const [leaving, setLeaving] = useState(false);
@@ -51,13 +51,7 @@ function Home() {
         </Loader>
       ) : (
         <>
-          <Banner
-            onClick={increaseIndex}
-            bgPhoto={makeImagePath(nowPlaying?.results[0].backdrop_path || "")}
-          >
-            <Title>{nowPlaying?.results[0].title}</Title>
-            <Overview>{nowPlaying?.results[0].overview}</Overview>
-          </Banner>
+          <Banner increaseIndex={increaseIndex} nowPlaying={nowPlaying} />
           <Slider
             index={index}
             data={nowPlaying}
@@ -87,28 +81,6 @@ const Loader = styled.div`
   span {
     font-size: 14px;
   }
-`;
-
-const Banner = styled.div<{ bgPhoto: string }>`
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  padding: 60px;
-  background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)),
-    url(${(props) => props.bgPhoto});
-  background-size: cover;
-`;
-
-const Title = styled.h2`
-  font-size: 72px;
-  font-weight: 700;
-  margin-bottom: 20px;
-`;
-
-const Overview = styled.p`
-  font-size: 24px;
-  width: 50%;
 `;
 
 export default Home;
