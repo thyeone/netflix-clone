1dc7d0e6def08d4d399d7b384be12699d66a5865
thyeone <thyeonee@gmail.com>
2023-01-28 15:47:09 +0900

    fix overlay problem

--- src/Components/Modal.tsx
+++ src/Components/Modal.tsx
@@ -310,6 +310,7 @@ const PlayButton = styled.button`
 const Overlay = styled(motion.div)`
   position: fixed;
   top: 0;
+  left: 0;
   width: 100%;
   height: 100%;
   background-color: rgba(0, 0, 0, 0.5);
